dff2bb14d4f0f8c51d390258233546633ea5b674
Robert Schoene <robert.schoene@tu-dresden.de>
2022-09-13 16:14:45 +0200

    first scatch for a soon to be architecture independent kernel to read remote modified cache lines

---
+++ kernel/memory/latencies/C/OpenMP/0/pointerchasing_m/pointerchasing_entry.c
@@ -0,0 +1,195 @@
+/********************************************************************
+ * BenchIT - Performance Measurement for Scientific Applications
+ * Contact: developer@benchit.org
+ *
+ * $Id: pointerchasing_entry.c 1 2009-09-11 12:26:19Z william $
+ * $URL: svn+ssh://william@rupert.zih.tu-dresden.de/svn-base/benchit-root/BenchITv6/kernel/memory/latencies/C/0/0/pointerchasing/pointerchasing_entry.c $
+ * For license details see COPYING in the package base directory
+ *******************************************************************/
+/* Kernel: Memory Access Time (C)
+ *******************************************************************/
+
+#include "interface.h"
+#include "pointerchasing.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <omp.h>
+#include <x86intrin.h>
+
+#define ONE {ptr=(void **) *ptr;}
+#define TEN ONE ONE ONE ONE ONE ONE ONE ONE ONE ONE
+#define HUN TEN TEN TEN TEN TEN TEN TEN TEN TEN TEN
+#define THO HUN HUN HUN HUN HUN HUN HUN HUN HUN HUN
+
+#define rdtsc(X)        asm volatile("rdtsc":"=A" (X))
+#if defined (__i386__)
+	#define rdtscll(val) do { \
+     unsigned int __a,__d; \
+     asm volatile("rdtsc" : "=a" (__a), "=d" (__d)); \
+     (val) = ((unsigned long long)__a) | (((unsigned long long)__d)<<32); \
+	} while(0)
+//	#define rdtscll(val)  asm volatile("rdtsc":"=A" (val))
+//__asm__ __volatile__("rdtsc" : "=A" (val))
+#elif defined(__x86_64__)
+	#define rdtscll(val) do { \
+     unsigned int __a,__d; \
+     asm volatile("rdtsc" : "=a" (__a), "=d" (__d)); \
+     (val) = ((unsigned long long)__a) | (((unsigned long long)__d)<<32); \
+	} while(0)
+#elif defined(__ia64__)
+#include <ia64intrin.h>
+	#define rdtscll(val) do { \
+     unsigned long __a; \
+     __a=__getReg(_IA64_REG_AR_ITC);\
+     (val) = __a * 4; \
+	} while(0)
+	//define rdtscll(val) val=(long long)(1.6E9*bi_gettime());//PAPI_get_real_cyc();
+#else
+	#define rdtscll(val) do { \
+	   asm volatile("mrs %0, PMCCNTR_EL0" : "=r"(val)); \
+	} while(0)
+#endif
+
+extern long cacheline_size;
+
+void *jump_around(void *mem, long n);
+
+
+/** generates a random number between 0 and (max-1)
+ *  @param  max maximum random number
+ *  @return a random number between 0 and (max-1)
+ */
+unsigned int random_number(unsigned long max)
+{
+  return (unsigned int) (((double)max)*rand()/(RAND_MAX+1.0));
+}
+
+
+/** creates a memory are that is randomly linked 
+ *  @param mem     the memory area to be used
+ *  @param length  the number of bytes that should be used
+ */
+void make_linked_memory(void *mem, long length) {
+
+  /* some pointers to generate the list */
+  void **ptr, **first;
+  /** how many ptr we create within the memory */
+  long num_ptr=length/cacheline_size; //sizeof(void *);
+  /** the list for all memory locations that are linked */
+  long *ptr_numbers;
+  /** for the loops */
+  long loop_ptrs;
+  /** actual random number */
+  long act_num;
+
+  /* allocate memory for ptr numbers */
+  ptr_numbers=(long *) malloc(num_ptr*sizeof(long));
+  if(num_ptr>0 && ptr_numbers==NULL)
+    {
+      printf("no more core in make_linked_mem()\n");
+      bi_cleanup(mem);
+      exit(1);
+    }
+  /* initialize ptr numbers, the 0 is used as the first
+   * number
+   */
+
+  for(loop_ptrs=1; loop_ptrs<num_ptr; loop_ptrs++)
+    ptr_numbers[loop_ptrs-1]=loop_ptrs;
+
+  /* init first ptr with first memory location */
+  ptr=(void **)mem;
+  first=ptr;
+   
+  num_ptr--;
+
+  while(num_ptr>1) {
+    /* get a random position within the
+       remaining list */
+    act_num=random_number(num_ptr);
+    /* create a link from the last ptr 
+       to this ptr */
+    *ptr=(void *) (first+(cacheline_size/sizeof(void**))*ptr_numbers[act_num]);
+    /* move pointer to new memory location */
+    ptr=first+(cacheline_size/sizeof(void**))*ptr_numbers[act_num];
+    /* remove used ptr number from list of
+       pointer numbers, just copies the last 
+       number to the actual position */
+    ptr_numbers[act_num]=ptr_numbers[num_ptr-1];
+    num_ptr--;
+  }
+
+  /* the last number is linked to the first */
+  *ptr=(void *) first;
+
+  /* free the ptr list */
+  free(ptr_numbers);
+  IDL(4,printf("\n"));
+}
+
+int ext_res;
+
+void flush()
+{
+  static int* fill=NULL;
+	extern long maxlength;
+	if (fill==NULL)
+  { fill=malloc(maxlength);
+    int x=rand();
+     for (long i=0;i<maxlength/sizeof(int);i++)
+       fill[i]=x*i;
+  }
+  unsigned int res=0;
+  for (long i=0;i<maxlength/sizeof(int);i++)
+     res =res ^ fill[i];
+  ext_res=res;
+}
+
+int bi_entry(void *mcb,int problemSize,double *results) {
+
+	static double timeroh=0, calloh=0;
+	long long start, stop;
+	int i;
+
+	long length;
+	long long c[4];
+	void *ptr;
+	
+	extern double dMemFactor;
+	extern long minlength;
+
+	length = (long)(((double)minlength)*pow(dMemFactor, (problemSize-1)));
+
+	results[0]=(double) length;
+
+
+omp_set_num_threads(2);
+#pragma omp parallel
+{
+  if (omp_get_thread_num() == 0) {
+  	make_linked_memory(mcb, length);
+  }
+#pragma omp barrier
+  jump_around(mcb, length/cacheline_size);
+  flush();
+  if (omp_get_thread_num() == 0) {
+  	make_linked_memory(mcb, length);
+  	jump_around(mcb, length/cacheline_size);
+  }
+#pragma omp barrier
+  _mm_mfence();
+  if (omp_get_thread_num() == 1) {
+    rdtscll(start);
+	  jump_around(mcb, length/cacheline_size);
+    _mm_lfence();
+    rdtscll(stop);
+	}
+}
+	 results[1]=(double)(stop-start)/((double)length/cacheline_size);
+   
+
+  return (0);
+}
---
+++ kernel/memory/latencies/C/OpenMP/0/pointerchasing_m/pointerchasing_init.c
@@ -0,0 +1,145 @@
+/********************************************************************
+ * BenchIT - Performance Measurement for Scientific Applications
+ * Contact: developer@benchit.org
+ *
+ * $Id: pointerchasing_init.c 1 2009-09-11 12:26:19Z william $
+ * $URL: svn+ssh://william@rupert.zih.tu-dresden.de/svn-base/benchit-root/BenchITv6/kernel/memory/latencies/C/0/0/pointerchasing/pointerchasing_init.c $
+ * For license details see COPYING in the package base directory
+ *******************************************************************/
+/* Kernel: Memory Access Time (C)
+ *******************************************************************/
+
+#include "interface.h"
+#include "pointerchasing.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+
+#if BENCHIT_KERNEL_USE_HUGE_PAGES == 1
+#include <sys/mman.h>
+#endif
+
+#ifndef BENCHIT_KERNEL_MIN_ACCESS_LENGTH
+#define BENCHIT_KERNEL_MIN_ACCESS_LENGTH (2048)
+#endif
+
+#ifndef BENCHIT_KERNEL_MAX_ACCESS_LENGTH
+#define BENCHIT_KERNEL_MAX_ACCESS_LENGTH (1024*1024)
+#endif
+
+#ifndef BENCHIT_KERNEL_ACCESS_STEPS
+#define BENCHIT_KERNEL_ACCESS_STEPS (100)
+#endif
+
+#ifndef BENCHIT_KERNEL_CACHELINE_SIZE
+#define BENCHIT_KERNEL_DEFAULT_CACHELINE_SIZE (128)
+#endif
+
+#ifndef BENCHIT_KERNEL_NUMBER_OF_JUMPS
+#define BENCHIT_KERNEL_NUMBER_OF_JUMPS (4000000)
+#endif
+
+
+unsigned int random_number(unsigned long max);
+void make_linked_memory(void *mem, long count);
+void init_global_vars(void);
+
+long minlength, maxlength, accessstride, numjumps;
+double dMemFactor;
+long nMeasurements;
+
+static int use_hugepages;
+long cacheline_size;
+
+void bi_getinfo(bi_info* infostruct){
+  int i;
+  char buf[200], *s;
+ 
+
+  init_global_vars();
+  /*infostruct->kernelstring=bi_strdup("Random Memory Access");*/
+  infostruct->kerneldescription = bi_strdup("Memory Access Time (C)");
+  infostruct->codesequence=bi_strdup("for i=1,N#  var=memory[random(0..size)]#");
+  infostruct->xaxistext=bi_strdup("Accessed Memory in Byte");
+  
+  infostruct->numfunctions= 1;
+  infostruct->num_measurements=nMeasurements;
+  
+	/* allocating memory for y axis texts and properties */
+  allocYAxis(infostruct);
+  
+  for (i=0; i< infostruct->numfunctions; i++)
+  		infostruct->selected_result[i] = SELECT_RESULT_AVERAGE;
+		
+  infostruct->base_xaxis=10.0;
+  infostruct->base_yaxis[0]=0.0;  
+  infostruct->legendtexts[0]=bi_strdup("Average Access Time");
+  infostruct->yaxistexts[0]=bi_strdup("s");
+}
+
+void init_global_vars() {
+    
+  char *envir;
+    
+
+  IDL(3,printf("Init global variables ... "));
+  envir=bi_getenv("BENCHIT_KERNEL_MIN_ACCESS_LENGTH",1);
+  minlength=(envir != 0) ? 1000*atoi(envir) : BENCHIT_KERNEL_MIN_ACCESS_LENGTH;
+  if(minlength==0) {
+    minlength=BENCHIT_KERNEL_MIN_ACCESS_LENGTH;
+  }
+  envir=0;
+  envir=bi_getenv("BENCHIT_KERNEL_MAX_ACCESS_LENGTH",1);
+  maxlength=(envir != 0) ? 1000*atoi(envir) : BENCHIT_KERNEL_MAX_ACCESS_LENGTH;
+  if(maxlength==0) {
+    maxlength=BENCHIT_KERNEL_MIN_ACCESS_LENGTH;
+  }
+  
+  envir=0;
+  envir=bi_getenv("BENCHIT_KERNEL_CACHELINE_SIZE",1);
+  cacheline_size=(envir != 0) ? atoi(envir) : BENCHIT_KERNEL_DEFAULT_CACHELINE_SIZE;
+  if(cacheline_size==0) {
+    cacheline_size=BENCHIT_KERNEL_DEFAULT_CACHELINE_SIZE;
+  }
+  
+  envir=0;
+  envir=bi_getenv("BENCHIT_KERNEL_ACCESS_STEPS",1);
+  nMeasurements = (envir != 0) ? atoi(envir) : BENCHIT_KERNEL_ACCESS_STEPS;
+  dMemFactor =((double)maxlength)/((double)minlength);
+  dMemFactor = pow(dMemFactor, 1.0/((double)nMeasurements-1));
+  
+  envir=bi_getenv("BENCHIT_KERNEL_USE_HUGE_PAGES",1);
+  use_hugepages=(envir != 0) ? atoi(envir) : 0;
+  IDL(3,printf("done\n"));
+}
+
+BI_GET_CALL_OVERHEAD_FUNC((),jump_around(NULL,0))
+
+void *bi_init(int problemSizemax){
+  void *mem;
+
+  IDL(3, printf("Enter init ... "));
+#if BENCHIT_KERNEL_USE_HUGE_PAGES == 1
+  if (use_hugepages) {
+    mem = mmap(NULL, maxlength*2, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    madvise(mem, maxlength*2, MADV_HUGEPAGE);
+    if (mem==NULL){
+      printf("No more core, need %.3f MByte\n", 
+	     (double)maxlength);
+      exit(127);
+    }
+  } else
+#endif  
+  {
+    mem = malloc(maxlength*2);
+  }
+  IDL(3, printf("allocated %.3f MByte\n",
+		(double)maxlength));
+  return (mem);
+}
+
+void bi_cleanup(void *mcb){
+  return;
+}
